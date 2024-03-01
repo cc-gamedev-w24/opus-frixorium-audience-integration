914e2240fd006822bf45a5c8c87bed4e257e0f64
Ethan Major <ethanmajor9@gmail.com>
2024-03-01 18:34:32 -0500

    Removed express and switched to WebSockets

--- audience_server.js
+++ audience_server.js
@@ -1,14 +1,31 @@
-const express = require('express');
-const app = express();
+const WebSocket = require('ws');
 const PORT = process.env.PORT || 3000
+const wss = new WebSocket.Server({ port:PORT });
 
 // Routes and middleware
 
-app.get('/test', (request, response) => {
-	console.log('Get request received');
-	response.send('Audience Integration');
-})
+wss.on('connection', function connection(ws, req) {
+	const ip = req.connection.remoteAddress;
 
-app.listen(PORT, () => {
-	console.log(`Server is running on port ${PORT}`);
+	console.log(`Client connected from IP: ${ip}}`);
+	
+	ws.on('error', (error) => {
+		console.log(`WebSocket error from IP: ${ip}:`, error);
+	});
+
+	ws.on('message', (message) => {
+		if(message == 'Unity') {
+			// Handle setting up game
+			console.log(`Unity client connected`);
+		} else {
+			// Handle connecting client
+			console.log('Audience client connected');
+		}
+	});
+
+	ws.on('close', () => {
+		console.log(`Client from IP: ${ip} disconnected`);
+	});
 });
+
+console.log(`Server is running on port: ${PORT}`);
